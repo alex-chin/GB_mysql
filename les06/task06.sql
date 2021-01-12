2ceb7455caf71cc93956069a3d84bbe8dab324ec
Alex Klyanchin <k142@mail.ru>
2021-01-13 00:23:01 +0300

    sql: les06 task2

--- les06/task06.sql
+++ les06/task06.sql
@@ -7,6 +7,12 @@
 
 SET @ID_USER = 1;
 SELECT COUNT(*) num_mes, from_user_id
-from messages WHERE to_user_id = @ID_USER
+FROM messages
+WHERE to_user_id = @ID_USER
 GROUP BY from_user_id
-ORDER BY num_mes DESC LIMIT 1;
+ORDER BY num_mes DESC
+LIMIT 1;
+
+# 2. Подсчитать общее количество лайков, которые получили пользователи младше 10 лет..
+
+SELECT user_id FROM profiles WHERE TIMESTAMPDIFF(YEAR, birthday, NOW()) < 10;
