c8b7e2c8a886377a18467262d6a1ded7160d914c
Alex Klyanchin <k142@mail.ru>
2021-01-29 19:04:36 +0300

    CW WS

--- course_work/is_sec.sql
+++ course_work/is_sec.sql
@@ -6,6 +6,7 @@ drop table if exists organizations;
 create table organizations
 (
     id          SERIAL PRIMARY KEY,
+    photo_id    bigint unsigned comment 'лого',
     name        varchar(255) null,
     short_name  varchar(255) null,
     address     varchar(255) null,
@@ -28,8 +29,8 @@ create table persons
     phone           varchar(255)         null,
     email           varchar(255)         null,
     position_sign   varchar(255)         null comment 'должность при подписании',
-    is_chairman     tinyint(1) default 0 not null comment 'председатель',
-    is_member       tinyint(1) default 0 not null comment 'член комиссии',
+    is_chairman     bit        default 0 not null comment 'председатель',
+    is_member       bit        default 0 not null comment 'член комиссии',
     is_agreement    tinyint(1) default 0 not null comment 'согласующий',
     is_approval     tinyint(1) default 0 not null comment 'утверждающий'
 )
@@ -49,23 +50,64 @@ create table infosystems
     before_num_attestation varchar(255)         null,
     is_ispdn               bit                  null comment 'система обработки персональных данных',
     ispdn_lev              int                  null comment 'Уровень_Защищенности',
-    is_gis                 bit                  null comment 'государственная информац система',
+    is_gis                 bit                  null comment 'государственная информационная система',
     gis_class              int                  null comment 'Класс_Защищенности',
     is_as                  bit                  null comment 'АС',
     as_class               int                  null comment 'Класс_Защищенности',
     date_agreement         date                 null comment 'дата согласования',
     date_approval          date                 null comment 'дата утверждения',
     reg_number             varchar(255)         null comment 'учетный номер',
-    organization_id        bigint unsigned      not null,
-    attestation_org_id     bigint unsigned      not null
+    organization_id        bigint unsigned      not null comment 'Организация владелец',
+    executor_org_id        bigint unsigned      not null comment 'Организация исполнитель по проекту защиты'
 )
     comment 'Информационная система';
 
 drop table if exists sites;
 create table sites
 (
-    id      serial primary key,
-    address varchar(266)
+    id             serial primary key,
+    infosystems_id bigint unsigned,
+    address        varchar(266)
 
-) comment 'Площадка, здание, адрес местоположения ИС';
+)
+    comment 'Площадка, здание, адрес местоположения ИС';
+
+drop table if exists photos;
+create table photos
+(
+    id           serial primary key,
+    site_id      bigint unsigned null comment 'скриншот испытаний на площадке',
+    workplace_id bigint unsigned null comment 'скриншот испытаний на рабочем месте',
+    is_logo      bit default 0 comment 'логотип для использования в документах',
+    file_name    char(20)        not null
+)
+    comment 'фотографии: лого, скриншоты испытаний';
+
+drop table if exists work_place;
+create table work_place
+(
+    id      serial primary key,
+    site_id bigint unsigned not null,
+    name    varchar(255)
+)
+    comment 'АРМ, Компьютер в сборе, периферия, стойка';
+
+drop table if exists unit;
+create table unit
+(
+    id        serial primary key,
+    site_id   bigint unsigned not null,
+    type_code char(20)        not null,
+    name      varchar(255),
+    part_num  char(100)
+)
+    comment 'отдельное устройство: монитор, системный блок, коммутатор';
+
+drop table if exists type_it;
+create table type_it
+(
+    code char(20) primary key
+)
+
+
 
